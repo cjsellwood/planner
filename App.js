b742ec365be96bd3a516b7736eac7242322492a8
cjsellwood <c.sellwood22@gmail.com>
2021-08-19 10:14:07 +1000

    Set up redux

--- App.js
+++ App.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-redux";
+import { applyMiddleware, combineReducers, createStore } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "remote-redux-devtools";
 
-export default function App() {
+import global from "./src/store/reducers/global";
+
+const rootReducer = combineReducers({
+  global: global,
+});
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Planner</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>Planner</Text>
+      </View>
+    </Provider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -15,6 +32,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
-    indent: ["error", 2],
   },
 });
+
+export default App;
