64b5c52ccacf8610e8713164ecc754308c36847a
cjsellwood <c.sellwood22@gmail.com>
2021-08-23 10:43:58 +1000

    Timer display now always showing correctly formatted timer

--- src/components/Timer/Circle.js
+++ src/components/Timer/Circle.js
@@ -5,31 +5,55 @@ import { useSelector } from "react-redux";
 const Circle = () => {
   const { timer } = useSelector((state) => state.timer);
 
+  let hour = Math.floor(timer / 1000 / 60 / 60);
+  let minute = Math.floor(timer / 1000 / 60) % 60;
+  let second = Math.ceil(timer / 1000) % 60;
+
+  let hourDisplay = hour + ":";
+  let minuteDisplay = minute + ":";
+  let secondDisplay = second;
+
+  // Don't show hour if 0
+  if (hour === 0) {
+    hourDisplay = "";
+    // If second is 0 and minute is 0 and hour is more than 0, add 1 hour
+  } else if (second === 0 && minute === 59 && hour > 0) {
+    hourDisplay = hour + 1 + ":";
+  }
+
+  // If second is 0  and hour is more than 0, add 1 to minute
+  if (second === 0 && minute === 59 && hour > 0) {
+    minuteDisplay = "00:";
+  } else if (second !== 0 && minute === 0 && hour === 0) {
+    // Don't show minute if 0 when hour is also 0
+    minuteDisplay = "";
+  } else if (second === 0) {
+    // If second is 0 add 1 to it
+    minuteDisplay = (minute + 1).toString().padStart(2, "0") + ":";
+  } else if (hour > 0) {
+    // Add leading 0 if also hours shown
+    minuteDisplay = minute.toString().padStart(2, "0") + ":";
+  }
+
+  // Add leading 0 if also minutes or hours shown
+  if (second === 0 || minute > 0 || hour > 0) {
+    secondDisplay = second.toString().padStart(2, "0");
+  }
+
+  console.log(
+    hour,
+    minute,
+    second,
+    hourDisplay + minuteDisplay + secondDisplay
+  );
+
   return (
     <View style={styles.circleContainer}>
       <Pressable onPress={() => {}} style={styles.circle}>
         <View style={styles.timeContainer}>
-          {timer >= 1000 * 60 * 60 ? (
-            <Text style={styles.text}>
-              {Math.floor(timer / 1000 / 60 / 60)}:
-            </Text>
-          ) : null}
-          {timer >= 1000 * 60 * 60 ? (
-            <Text style={styles.text}>
-              {new Date(timer).getMinutes().toString().padStart(2, "0")}:
-            </Text>
-          ) : timer >= 1000 * 60 ? (
-            <Text style={styles.text}>{new Date(timer).getMinutes()}:</Text>
-          ) : null}
-          {timer >= 1000 * 60 ? (
-            <Text style={styles.text}>
-              {new Date(timer).getSeconds().toString().padStart(2, "0")}
-            </Text>
-          ) : (
-            <Text style={styles.text}>
-              {Math.ceil(new Date(timer).getSeconds())}
-            </Text>
-          )}
+          <Text style={styles.text}>{hourDisplay}</Text>
+          <Text style={styles.text}>{minuteDisplay}</Text>
+          <Text style={styles.text}>{secondDisplay}</Text>
         </View>
       </Pressable>
     </View>
